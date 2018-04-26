99ade7b2729281f218bd8b1a60f9ede7f0772747
Phonpaseuth Khamsenesouk <phonpaseuth@gmail.com>
2018-04-26 15:41:46 -0500

    style category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,12 @@
 class CategoriesController < ApplicationController
+	before_action :require_user
 	
 	def home
 		@categories = Category.all
 	end
 
 	def show_category
+		@user = @current_user
 		@category = Category.find(params[:id])
 		@posts = @category.posts.order(updated_at: :desc).paginate(:page => params[:page], :per_page => 5)
 	end
